f28ed55a3e17071cbcea491829ab7180bc58037e
David Bernard <dbernardcoding@gmail.com>
2021-04-14 17:04:56 -0400

    cut amount of code almost in half

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -9,18 +9,14 @@ function getApi() {
         return response.json();
         })
         .then(function (data) {
-            let currentName = document.querySelector('#name');
-            let currentTemp = document.querySelector('#temp');
-            let currentWind = document.querySelector('#wind');
-            let currentHumidity = document.querySelector('#humidity');
             let listItem = document.createElement('button');
 
             listItem.classList.add("previousBtn");
             listItem.textContent = inputSearch.value;
-            currentName.textContent = data['name'];
-            currentTemp.textContent = data['main']['temp'];
-            currentWind.textContent = data['wind']['speed'];
-            currentHumidity.textContent = data['main']['humidity'];
+            document.querySelector('#name').textContent = data['name'];
+            document.querySelector('#temp').textContent = data['main']['temp'];
+            document.querySelector('#wind').textContent = data['wind']['speed'];
+            document.querySelector('#humidity').textContent = data['main']['humidity'];
             repoList.appendChild(listItem);
             return fetch("https://api.openweathermap.org/data/2.5/onecall?lat=" + data['coord']['lat'] + "&lon=" + data['coord']['lon'] + "&appid=0a4b3b2dc3abb0dca1990864d02586cd");
         })  
@@ -32,75 +28,40 @@ function getApi() {
             
             currentUv.textContent = data['current']['uvi'];
 
-            let weekDayOne = document.querySelector('#weekDayOne')
-            let dayOne = document.querySelector('#dayOne');
-            let iconOne = document.querySelector('#iconOne');
-            let tempOne = document.querySelector('#tempOne');
-            let windOne = document.querySelector('#windOne');
-            let humidityOne = document.querySelector('#humidityOne');
+            document.querySelector('#weekDayOne').textContent = moment.unix(data['daily'][1]['dt']).format("dddd");
+            document.querySelector('#dayOne').textContent = moment.unix(data['daily'][1]['dt']).format("MMMM Do YYYY");
+            document.querySelector('#iconOne').setAttribute('src', "http://openweathermap.org/img/w/" + data['daily'][1]['weather'][0]['icon'] + ".png");
+            document.querySelector('#tempOne').textContent = "Temperature: " + data['daily'][1]['temp']['day'];
+            document.querySelector('#windOne').textContent = "Wind Speed: " + data['daily'][1]['wind_speed'];
+            document.querySelector('#humidityOne').textContent = "Humidity: " + data['daily'][1]['humidity'];
 
-            weekDayOne.textContent = moment.unix(data['daily'][1]['dt']).format("dddd");
-            dayOne.textContent = moment.unix(data['daily'][1]['dt']).format("MMMM Do YYYY");
-            iconOne.setAttribute('src', "http://openweathermap.org/img/w/" + data['daily'][1]['weather'][0]['icon'] + ".png");
-            tempOne.textContent = "Temperature: " + data['daily'][1]['temp']['day'];
-            windOne.textContent = "Wind Speed: " + data['daily'][1]['wind_speed'];
-            humidityOne.textContent = "Humidity: " + data['daily'][1]['humidity'];
+            document.querySelector('#weekDayTwo').textContent = moment.unix(data['daily'][2]['dt']).format("dddd");
+            document.querySelector('#dayTwo').textContent = moment.unix(data['daily'][2]['dt']).format("MMMM Do YYYY");
+            document.querySelector('#iconTwo').setAttribute('src', "http://openweathermap.org/img/w/" + data['daily'][2]['weather'][0]['icon'] + ".png");
+            document.querySelector('#tempTwo').textContent = "Temperature: " + data['daily'][2]['temp']['day'];
+            document.querySelector('#windTwo').textContent = "Wind Speed: " + data['daily'][2]['wind_speed'];
+            document.querySelector('#humidityTwo').textContent = "Humidity: " + data['daily'][2]['humidity'];
 
-            let weekDayTwo = document.querySelector('#weekDayTwo')
-            let dayTwo = document.querySelector('#dayTwo');
-            let iconTwo = document.querySelector('#iconTwo');
-            let tempTwo = document.querySelector('#tempTwo');
-            let windTwo = document.querySelector('#windTwo');
-            let humidityTwo = document.querySelector('#humidityTwo');
+            document.querySelector('#weekDayThree').textContent = moment.unix(data['daily'][3]['dt']).format("dddd");
+            document.querySelector('#dayThree').textContent = moment.unix(data['daily'][3]['dt']).format("MMMM Do YYYY");
+            document.querySelector('#iconThree').setAttribute('src', "http://openweathermap.org/img/w/" + data['daily'][3]['weather'][0]['icon'] + ".png");
+            document.querySelector('#tempThree').textContent = "Temperature: " + data['daily'][3]['temp']['day'];
+            document.querySelector('#windThree').textContent = "Wind Speed: " + data['daily'][3]['wind_speed'];
+            document.querySelector('#humidityThree').textContent = "Humidity: " + data['daily'][3]['humidity'];
 
-            weekDayTwo.textContent = moment.unix(data['daily'][2]['dt']).format("dddd");
-            dayTwo.textContent = moment.unix(data['daily'][2]['dt']).format("MMMM Do YYYY");
-            iconTwo.setAttribute('src', "http://openweathermap.org/img/w/" + data['daily'][2]['weather'][0]['icon'] + ".png");
-            tempTwo.textContent = "Temperature: " + data['daily'][2]['temp']['day'];
-            windTwo.textContent = "Wind Speed: " + data['daily'][2]['wind_speed'];
-            humidityTwo.textContent = "Humidity: " + data['daily'][2]['humidity'];
+            document.querySelector('#weekDayFour').textContent = moment.unix(data['daily'][4]['dt']).format("dddd");
+            document.querySelector('#dayFour').textContent = moment.unix(data['daily'][4]['dt']).format("MMMM Do YYYY");
+            document.querySelector('#iconFour').setAttribute('src', "http://openweathermap.org/img/w/" + data['daily'][4]['weather'][0]['icon'] + ".png");
+            document.querySelector('#tempFour').textContent = "Temperature: " + data['daily'][4]['temp']['day'];
+            document.querySelector('#windFour').textContent = "Wind Speed: " + data['daily'][4]['wind_speed'];
+            document.querySelector('#humidityFour').textContent = "Humidity: " + data['daily'][4]['humidity'];
 
-            let weekDayThree = document.querySelector('#weekDayThree')
-            let dayThree = document.querySelector('#dayThree');
-            let iconThree = document.querySelector('#iconThree');
-            let tempThree = document.querySelector('#tempThree');
-            let windThree = document.querySelector('#windThree');
-            let humidityThree = document.querySelector('#humidityThree');
-
-            weekDayThree.textContent = moment.unix(data['daily'][3]['dt']).format("dddd");
-            dayThree.textContent = moment.unix(data['daily'][3]['dt']).format("MMMM Do YYYY");
-            iconThree.setAttribute('src', "http://openweathermap.org/img/w/" + data['daily'][3]['weather'][0]['icon'] + ".png");
-            tempThree.textContent = "Temperature: " + data['daily'][3]['temp']['day'];
-            windThree.textContent = "Wind Speed: " + data['daily'][3]['wind_speed'];
-            humidityThree.textContent = "Humidity: " + data['daily'][3]['humidity'];
-
-            let weekDayFour = document.querySelector('#weekDayFour')
-            let dayFour = document.querySelector('#dayFour');
-            let iconFour = document.querySelector('#iconFour');
-            let tempFour = document.querySelector('#tempFour');
-            let windFour = document.querySelector('#windFour');
-            let humidityFour = document.querySelector('#humidityFour');
-
-            weekDayFour.textContent = moment.unix(data['daily'][4]['dt']).format("dddd");
-            dayFour.textContent = moment.unix(data['daily'][4]['dt']).format("MMMM Do YYYY");
-            iconFour.setAttribute('src', "http://openweathermap.org/img/w/" + data['daily'][4]['weather'][0]['icon'] + ".png");
-            tempFour.textContent = "Temperature: " + data['daily'][4]['temp']['day'];
-            windFour.textContent = "Wind Speed: " + data['daily'][4]['wind_speed'];
-            humidityFour.textContent = "Humidity: " + data['daily'][4]['humidity'];
-
-            let weekDayFive = document.querySelector('#weekDayFive')
-            let dayFive = document.querySelector('#dayFive');
-            let iconFive = document.querySelector('#iconFive');
-            let tempFive = document.querySelector('#tempFive');
-            let windFive = document.querySelector('#windFive');
-            let humidityFive = document.querySelector('#humidityFive');
-
-            weekDayFive.textContent = moment.unix(data['daily'][5]['dt']).format("dddd");
-            dayFive.textContent = moment.unix(data['daily'][5]['dt']).format("MMMM Do YYYY");
-            iconFive.setAttribute('src', "http://openweathermap.org/img/w/" + data['daily'][5]['weather'][0]['icon'] + ".png");
-            tempFive.textContent = "Temperature: " + data['daily'][5]['temp']['day'];
-            windFive.textContent = "Wind Speed: " + data['daily'][5]['wind_speed'];
-            humidityFive.textContent = "Humidity: " + data['daily'][5]['humidity'];
+            document.querySelector('#weekDayFive').textContent = moment.unix(data['daily'][5]['dt']).format("dddd");
+            document.querySelector('#dayFive').textContent = moment.unix(data['daily'][5]['dt']).format("MMMM Do YYYY");
+            document.querySelector('#iconFive').setAttribute('src', "http://openweathermap.org/img/w/" + data['daily'][5]['weather'][0]['icon'] + ".png");
+            document.querySelector('#tempFive').textContent = "Temperature: " + data['daily'][5]['temp']['day'];
+            document.querySelector('#windFive').textContent = "Wind Speed: " + data['daily'][5]['wind_speed'];
+            document.querySelector('#humidityFive').textContent = "Humidity: " + data['daily'][5]['humidity'];
         })  
 }   
 
