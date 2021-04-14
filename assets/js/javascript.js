81a4715dd49057d23510ecc787a1188233cacce0
David Bernard <dbernardcoding@gmail.com>
2021-04-14 09:44:47 -0400

    added content to all five day divs

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -11,7 +11,7 @@ let dayThreeDiv = document.querySelector('#dayThreeDiv');
 let dayFourDiv = document.querySelector('#dayFourDiv');
 let dayFiveDiv = document.querySelector('#dayFiveDiv');
 let dayOne = document.querySelector('#dayOne');
-let iconImageOne = document.querySelector('#iconOne');
+let iconOne = document.querySelector('#iconOne');
 let tempOne = document.querySelector('#tempOne');
 let windOne = document.querySelector('#windOne');
 let humidityOne = document.querySelector('#humidityOne');
@@ -39,7 +39,6 @@ let fetchButton = document.querySelector('.searchButton');
 
 function getApi() {
     let requestUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + inputSearch.value + "&appid=0a4b3b2dc3abb0dca1990864d02586cd";
-    console.log(requestUrl);
     fetch(requestUrl)
         .then(function (response) {
         return response.json();
@@ -60,14 +59,37 @@ function getApi() {
         return response.json();
         })
         .then(function (data) {
-            console.log(data);
             currentUv.textContent = data['current']['uvi'];
+
             dayOne.textContent = moment.unix(data['daily'][1]['dt']).format("dddd, MMMM Do YYYY");
-            tempOne.textContent = data['daily'][1]['temp']['day'];
-            iconImageOne.setAttribute('src', "http://openweathermap.org/img/w/" + data['daily'][1]['weather'][0]['icon'] + ".png");
-            windOne.textContent = data['daily'][1]['wind_speed'];
-            humidityOne.textContent = data['daily'][1]['humidity'];
-            console.log(iconOne);
+            iconOne.setAttribute('src', "http://openweathermap.org/img/w/" + data['daily'][1]['weather'][0]['icon'] + ".png");
+            tempOne.textContent = "Temperature: " + data['daily'][1]['temp']['day'];
+            windOne.textContent = "Wind Speed: " + data['daily'][1]['wind_speed'];
+            humidityOne.textContent = "Humidity: " + data['daily'][1]['humidity'];
+
+            dayTwo.textContent = moment.unix(data['daily'][2]['dt']).format("dddd, MMMM Do YYYY");
+            iconTwo.setAttribute('src', "http://openweathermap.org/img/w/" + data['daily'][2]['weather'][0]['icon'] + ".png");
+            tempTwo.textContent = "Temperature: " + data['daily'][2]['temp']['day'];
+            windTwo.textContent = "Wind Speed: " + data['daily'][2]['wind_speed'];
+            humidityTwo.textContent = "Humidity: " + data['daily'][2]['humidity'];
+
+            dayThree.textContent = moment.unix(data['daily'][3]['dt']).format("dddd, MMMM Do YYYY");
+            iconThree.setAttribute('src', "http://openweathermap.org/img/w/" + data['daily'][3]['weather'][0]['icon'] + ".png");
+            tempThree.textContent = "Temperature: " + data['daily'][3]['temp']['day'];
+            windThree.textContent = "Wind Speed: " + data['daily'][3]['wind_speed'];
+            humidityThree.textContent = "Humidity: " + data['daily'][3]['humidity'];
+
+            dayFour.textContent = moment.unix(data['daily'][4]['dt']).format("dddd, MMMM Do YYYY");
+            iconFour.setAttribute('src', "http://openweathermap.org/img/w/" + data['daily'][4]['weather'][0]['icon'] + ".png");
+            tempFour.textContent = "Temperature: " + data['daily'][4]['temp']['day'];
+            windFour.textContent = "Wind Speed: " + data['daily'][4]['wind_speed'];
+            humidityFour.textContent = "Humidity: " + data['daily'][4]['humidity'];
+
+            dayFive.textContent = moment.unix(data['daily'][5]['dt']).format("dddd, MMMM Do YYYY");
+            iconFive.setAttribute('src', "http://openweathermap.org/img/w/" + data['daily'][5]['weather'][0]['icon'] + ".png");
+            tempFive.textContent = "Temperature: " + data['daily'][5]['temp']['day'];
+            windFive.textContent = "Wind Speed: " + data['daily'][5]['wind_speed'];
+            humidityFive.textContent = "Humidity: " + data['daily'][5]['humidity'];
         })  
 }   
 
