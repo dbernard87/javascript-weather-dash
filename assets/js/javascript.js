6c942b47430a91022c19bb8ac5db01818b24a572
David Bernard <dbernardcoding@gmail.com>
2021-04-14 08:37:53 -0400

    adding content to five day forecast

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -5,6 +5,36 @@ let currentTemp = document.querySelector('#temp');
 let currentWind = document.querySelector('#wind');
 let currentHumidity = document.querySelector('#humidity');
 let currentUv = document.querySelector('#uv');
+let dayOneDiv = document.querySelector('#dayOneDiv');
+let dayTwoDiv = document.querySelector('#dayTwoDiv');
+let dayThreeDiv = document.querySelector('#dayThreeDiv');
+let dayFourDiv = document.querySelector('#dayFourDiv');
+let dayFiveDiv = document.querySelector('#dayFiveDiv');
+let dayOne = document.querySelector('#dayOne');
+let iconImageOne = document.querySelector('#iconOne');
+let tempOne = document.querySelector('#tempOne');
+let windOne = document.querySelector('#windOne');
+let humidityOne = document.querySelector('#humidityOne');
+let dayTwo = document.querySelector('#dayTwo');
+let iconTwo = document.querySelector('#iconTwo');
+let tempTwo = document.querySelector('#tempTwo');
+let windTwo = document.querySelector('#windTwo');
+let humidityTwo = document.querySelector('#humidityTwo');
+let dayThree = document.querySelector('#dayThree');
+let iconThree = document.querySelector('#iconThree');
+let tempThree = document.querySelector('#tempThree');
+let windThree = document.querySelector('#windThree');
+let humidityThree = document.querySelector('#humidityThree');
+let dayFour = document.querySelector('#dayFour');
+let iconFour = document.querySelector('#iconFour');
+let tempFour = document.querySelector('#tempFour');
+let windFour = document.querySelector('#windFour');
+let humidityFour = document.querySelector('#humidityFour');
+let dayFive = document.querySelector('#dayFive');
+let iconFive = document.querySelector('#iconFive');
+let tempFive = document.querySelector('#tempFive');
+let windFive = document.querySelector('#windFive');
+let humidityFive = document.querySelector('#humidityFive');
 let fetchButton = document.querySelector('.searchButton');
 
 function getApi() {
@@ -32,6 +62,12 @@ function getApi() {
         .then(function (data) {
             console.log(data);
             currentUv.textContent = data['current']['uvi'];
+            dayOne.textContent = moment.unix(data['daily'][1]['dt']).format("dddd, MMMM Do YYYY");
+            tempOne.textContent = data['daily'][1]['temp']['day'];
+            iconImageOne.setAttribute('src', "http://openweathermap.org/img/w/" + data['daily'][1]['weather'][0]['icon'] + ".png");
+            windOne.textContent = data['daily'][1]['wind_speed'];
+            humidityOne.textContent = data['daily'][1]['humidity'];
+            console.log(iconOne);
         })  
 }   
 
