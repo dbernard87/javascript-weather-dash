e197e280890ae6c94a3d923b9721852716f83815
David Bernard <dbernardcoding@gmail.com>
2021-04-14 04:21:40 -0400

    updating fetch API

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,5 +1,10 @@
 let inputSearch = document.querySelector('.locationSearch');
 let repoList = document.querySelector('ul');
+let currentName = document.querySelector('#name');
+let currentTemp = document.querySelector('#temp');
+let currentWind = document.querySelector('#wind');
+let currentHumidity = document.querySelector('#humidity');
+let currentUv = document.querySelector('#uv');
 let fetchButton = document.querySelector('.searchButton');
 
 function getApi() {
@@ -11,11 +16,23 @@ function getApi() {
         })
         .then(function (data) {
             console.log(data);
-            var listItem = document.createElement('button');
+            let listItem = document.createElement('button');
             listItem.classList.add("previousBtn");
             listItem.textContent = inputSearch.value;
+            currentName.textContent = data['name'];
+            currentTemp.textContent = data['main']['temp'];
+            currentWind.textContent = data['wind']['speed'];
+            currentHumidity.textContent = data['main']['humidity'];
             repoList.appendChild(listItem);
-    });
-}
+            return fetch("https://api.openweathermap.org/data/2.5/onecall?lat=" + data['coord']['lat'] + "&lon=" + data['coord']['lon'] + "&appid=0a4b3b2dc3abb0dca1990864d02586cd");
+        })  
+        .then(function (response) {
+        return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+            currentUv.textContent = data['current']['uvi'];
+        })  
+}   
 
 fetchButton.addEventListener('click', getApi);
