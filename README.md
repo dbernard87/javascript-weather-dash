61bbea1291636c196199199d3d54a172de3ee3fe
David Bernard <77427582+dbernard87@users.noreply.github.com>
2021-04-16 19:45:03 -0400

    final styling changes, adding readme

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
-# weather-watch
+# Weather Dash
+
+
+## Description
+
+This is a weather dashboard that displays the current forecast for the users searched location, as well as a five day forecast for the location. The users previously searched locations are appended to a button, and saved to local storage. The user can click the button to see the forcast for that location again. Clicking the red X button deletes the saved locations individually, starting with the oldest location searched.
+
+The UV Index has a background color that will change depending on the current UV index, with green being safest, and red being dangerous.
+
+The application was created using HTML, CSS, and Javascript. The application uses the Open Weather API, Font Awesome icons, Google Fonts, and Moment JS to display the current date.
+
+
+## Usage
+
+Enter the desired location in the search bar, and press submit, or hit enter. This will get the current forecast and a five day forecast of the location.
+
+[https://dbernard87.github.io/javascript-weather-dash/]
+
+![image of Weather Dash website homepage](assets/images/screenshot.png)
+
+
+## Features
+
+- Open Weather API
+- Current Forecast
+- Five Day Forecast
+- UV Index with color changing background.
+- Moment JS
+
+
+## Credits
+
+- David Bernard : [https://github.com/dbernard87]
+- Icons from Font Awesome : [https://fontawesome.com/]
+- Stack Overflow : [https://stackoverflow.com/]
+- w3schools : [https://www.w3schools.com/]
+- MDN Documentation : [https://developer.mozilla.org/en-US/]
+- Open Weather API [https://openweathermap.org/api]
--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,15 +1,18 @@
+/*-------------------- Global Variables --------------------*/
 let inputSearch = document.querySelector('.locationSearch');
 let previousSearch = document.querySelector('.previousSearch');
 let searchButton = document.querySelector('.searchButton');
 let clearButton = document.querySelector('.clearButton');
 let pastSearches = localStorage.getItem('location');
 
+/*------------ If Statement For Displaying Past Searches ------------*/
 if (pastSearches == null) {
     pastSearches = [];      
 } else {
     pastSearches = JSON.parse(pastSearches);
 }
 
+/*-------------------- Add Past Search Function --------------------*/
 function addPastSearch(city) {
     let listItem = document.createElement('button');
     listItem.addEventListener('click', function() {
@@ -27,6 +30,7 @@ for (let pastSearch of pastSearches) {
     addPastSearch(pastSearch);
 }
 
+/*-------------------- Get Weather and One Call API Function --------------------*/
 function getApi() {
     let requestUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + inputSearch.value + "&units=imperial&appid=0a4b3b2dc3abb0dca1990864d02586cd";
     fetch(requestUrl)
@@ -39,6 +43,7 @@ function getApi() {
                 pastSearches.push(inputSearch.value)
                 localStorage.setItem('location', JSON.stringify(pastSearches));
             }
+            /*-------------------- Current Weather Div Dynamic Styling --------------------*/
             document.querySelector('#currentSpan').textContent = inputSearch.value;
             document.querySelector('#fiveDaySpan').textContent = inputSearch.value;
             document.querySelector('#name').textContent = data.name;
@@ -51,11 +56,12 @@ function getApi() {
         return response.json();
         })
         .then(function (data) {
+            /*-------------------- UV Index Dynamic Styling --------------------*/
             document.querySelector('#currentDay').textContent = moment.unix(data.daily[0].dt).format("dddd");
             document.querySelector('#currentDate').textContent = moment.unix(data.daily[0].dt).format("MMMM Do YYYY");
             document.querySelector('#icon').setAttribute('src', "http://openweathermap.org/img/wn/" + data.daily[0].weather[0].icon + "@2x.png");
             document.querySelector('#uv').textContent = "UV Index: " + data.current.uvi;
-
+            /*-------------------- If Statement To Set UV Index Background Color --------------------*/
             if (data.current.uvi < 3) {
                 document.querySelector('#uv').setAttribute('style', 'background-color: #28a745;');
             } else if (data.current.uvi >= 3 && data.current.uvi <= 5) {
@@ -65,35 +71,35 @@ function getApi() {
             } else if (data.current.uvi >= 8 && data.current.uvi <= 10) {
                 document.querySelector('#uv').setAttribute('style', 'background-color: #dc3545;');
             }
-
+            /*-------------------- Day One Div Dynamic Styling --------------------*/
             document.querySelector('#weekDayOne').textContent = moment.unix(data.daily[1].dt).format("dddd");
             document.querySelector('#dayOne').textContent = moment.unix(data.daily[1].dt).format("MMMM Do YYYY");
             document.querySelector('#iconOne').setAttribute('src', "http://openweathermap.org/img/wn/" + data.daily[1].weather[0].icon + "@2x.png");
             document.querySelector('#tempOne').textContent = "Temperature: " + data.daily[1].temp.day + " °F";
             document.querySelector('#windOne').textContent = "Wind Speed: " + data.daily[1].wind_speed + " MPH";
             document.querySelector('#humidityOne').textContent = "Humidity: " + data.daily[1].humidity + "%";
-
+            /*-------------------- Day Two Div Dynamic Styling --------------------*/
             document.querySelector('#weekDayTwo').textContent = moment.unix(data.daily[2].dt).format("dddd");
             document.querySelector('#dayTwo').textContent = moment.unix(data.daily[2].dt).format("MMMM Do YYYY");
             document.querySelector('#iconTwo').setAttribute('src', "http://openweathermap.org/img/wn/" + data.daily[2].weather[0].icon + "@2x.png");
             document.querySelector('#tempTwo').textContent = "Temperature: " + data.daily[2].temp.day + " °F";
             document.querySelector('#windTwo').textContent = "Wind Speed: " + data.daily[2].wind_speed + " MPH";
             document.querySelector('#humidityTwo').textContent = "Humidity: " + data.daily[2].humidity + "%";
-
+            /*-------------------- Day Three Div Dynamic Styling --------------------*/
             document.querySelector('#weekDayThree').textContent = moment.unix(data.daily[3].dt).format("dddd");
             document.querySelector('#dayThree').textContent = moment.unix(data.daily[3].dt).format("MMMM Do YYYY");
             document.querySelector('#iconThree').setAttribute('src', "http://openweathermap.org/img/wn/" + data.daily[3].weather[0].icon + "@2x.png");
             document.querySelector('#tempThree').textContent = "Temperature: " + data.daily[3].temp.day + " °F";
             document.querySelector('#windThree').textContent = "Wind Speed: " + data.daily[3].wind_speed + " MPH";
             document.querySelector('#humidityThree').textContent = "Humidity: " + data.daily[3].humidity + "%";
-
+            /*-------------------- Day Four Div Dynamic Styling --------------------*/
             document.querySelector('#weekDayFour').textContent = moment.unix(data.daily[4].dt).format("dddd");
             document.querySelector('#dayFour').textContent = moment.unix(data.daily[4].dt).format("MMMM Do YYYY");
             document.querySelector('#iconFour').setAttribute('src', "http://openweathermap.org/img/wn/" + data.daily[4].weather[0].icon + "@2x.png");
             document.querySelector('#tempFour').textContent = "Temperature: " + data.daily[4].temp.day + " °F";
             document.querySelector('#windFour').textContent = "Wind Speed: " + data.daily[4].wind_speed + " MPH";
             document.querySelector('#humidityFour').textContent = "Humidity: " + data.daily[4].humidity + "%";
-
+            /*-------------------- Day Five Div Dynamic Styling --------------------*/
             document.querySelector('#weekDayFive').textContent = moment.unix(data.daily[5].dt).format("dddd");
             document.querySelector('#dayFive').textContent = moment.unix(data.daily[5].dt).format("MMMM Do YYYY");
             document.querySelector('#iconFive').setAttribute('src', "http://openweathermap.org/img/wn/" + data.daily[5].weather[0].icon + "@2x.png");
@@ -102,12 +108,14 @@ function getApi() {
             document.querySelector('#humidityFive').textContent = "Humidity: " + data.daily[5].humidity + "%";
         })  
 }   
-    
+
+/*-------------------- Clear Button Event Listener and Function --------------------*/    
 clearButton.addEventListener('click', function() {
     localStorage.removeItem('location');
-    previousSearch.textContent = "";
+    document.querySelector('.previousBtn').remove();
 })
 
+/*-------------------- Input Enter Key Down Event Listener and Function  --------------------*/
 document.querySelector('#mainInput').addEventListener('keydown', function(event) {
     if (event.keyCode == 13) {
         document.querySelector('#currentSpan').textContent = inputSearch.value;
@@ -116,4 +124,5 @@ document.querySelector('#mainInput').addEventListener('keydown', function(event)
     }
 });
 
+/*-------------------- Search Button Event Listener --------------------*/
 searchButton.addEventListener('click', getApi);
